c0a470fca8f5783476c31e7aa9709180dd767f47
Kaizen <gibrantsyareza@gmail.com>
2026-01-24 16:52:33 +0700

    adding user model and making one-to-many relationship to book, so 1 user can have many book

---
+++ api/model/user.model.go
@@ -0,0 +1,21 @@
+package model
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID uuid.UUID `json:"id" gorm:"primary_key"`
+	FirstName string `json:"first_name" gorm:"not null;unique"`
+	LastName string `json:"last_name" gorm:"not null;unique"`
+	Username string `json:"username" gorm:"unique;not null"`
+	Email string `json:"email" gorm:"unique;not null"` 
+	Password string `json:"-" gorm:"not null"`
+	Books []Book `json:"-"`
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	u.ID = uuid.New()
+	return
+}
