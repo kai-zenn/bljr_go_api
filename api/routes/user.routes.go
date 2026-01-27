75ef17f20157199eaa2ba70ac7cc42ff81f39016
Kaizen <gibrantsyareza@gmail.com>
2026-01-27 11:47:08 +0700

    adding user route for get all user, get user by id, and post/creating new user

---
+++ api/routes/user.routes.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"github.com/kai-zenn/bljr_go_api/api/controller"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoute(r *gin.Engine) {
+	userGroup := r.Group("/users")
+	{
+		userGroup.POST("", controller.CreateUser)
+		userGroup.GET("/:id", controller.GetUserById)
+		userGroup.GET("", controller.GetUsers)
+		userGroup.PUT("/:id", controller.UpdateUser)
+	}
+}
