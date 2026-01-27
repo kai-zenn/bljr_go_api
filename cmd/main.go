9f3c345cf05c5e91b93991883e1d7ffdf5d8fb1e
Kaizen <gibrantsyareza@gmail.com>
2026-01-27 11:47:35 +0700

    adding the user routes to main

--- cmd/main.go
+++ cmd/main.go
@@ -3,18 +3,22 @@ package main
 import (
 	"github.com/kai-zenn/bljr_go_api/api/configs"
 	"github.com/kai-zenn/bljr_go_api/api/routes"
+	"github.com/kai-zenn/bljr_go_api/api/migration"
 	"github.com/gin-gonic/gin"
 )
 
 func Init() {
 	configs.InitDB()
+	migration.Migrate()
 }
 
 func main() {
+
 	Init()
 	r := gin.Default()
 
 	routes.BookRoute(r)
+	routes.UserRoute(r)
 
-	r.Run(":3000")
+	r.Run()
 }
