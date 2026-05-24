082459212f92001aea61c7ded0add398558685be
Kaizen <gibrantsyareza@gmail.com>
2026-05-24 16:32:07 +0700

    adding auth middleware to make the book routes proteced

--- api/routes/book.route.go
+++ api/routes/book.route.go
@@ -1,17 +1,18 @@
 package routes
 
 import (
-	"github.com/kai-zenn/bljr_go_api/api/controller"
 	"github.com/gin-gonic/gin"
+	"github.com/kai-zenn/bljr_go_api/api/controller"
+	"github.com/kai-zenn/bljr_go_api/api/middlewares"
 )
 
 func BookRoute(r *gin.Engine) {
 	userGroup := r.Group("/books")
 	{
-		userGroup.POST("", controller.CreateBook)
-		userGroup.GET("/:id", controller.GetBook)
-		userGroup.GET("", controller.GetAllBooks)
-		userGroup.PUT("/:id", controller.UpdateBook)
-		userGroup.DELETE("/:id", controller.DeleteBook)
+		userGroup.POST("", middlewares.AuthMiddleware() ,controller.CreateBook)
+		userGroup.GET("/:id", middlewares.AuthMiddleware() , controller.GetBook)
+		userGroup.GET("", middlewares.AuthMiddleware() , controller.GetAllBooks)
+		userGroup.PUT("/:id",  middlewares.AuthMiddleware() ,controller.UpdateBook)
+		userGroup.DELETE("/:id",  middlewares.AuthMiddleware() ,controller.DeleteBook)
 	}
 }
