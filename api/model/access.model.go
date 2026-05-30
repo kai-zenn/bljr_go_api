52c41ae4c1c4aaf6301a034ec0d477820b7b7e20
Kaizen <gibrantsyareza@gmail.com>
2026-05-30 16:28:32 +0700

    fixing typo on the struct model

--- api/model/access.model.go
+++ api/model/access.model.go
@@ -3,5 +3,5 @@ package model
 type Access struct {
   AccessId uint `json:"id" gorm:"primaryKey;column:access_id"`
   AccessName string `json:"access_name"`
-  Roles []Role `gorm:"many2many:role_access;foreignKey:AccessId;joinForeignKey:AccessId;refrences:RoleId;joinRefrences:RoleId"`
+  Roles []Role `gorm:"many2many:role_access;foreignKey:AccessId;joinForeignKey:AccessId;references:RoleId;joinReferences:RoleId"`
 }
--- api/model/role.model.go
+++ api/model/role.model.go
@@ -3,6 +3,6 @@ package model
 type Role struct {
   RoleId uint `json:"id" gorm:"primaryKey;column:role_id"`
   RoleName string `json:"role_name"`
-  Accesses []Access `gorm:"many2many:role_access;foreignKey;RoleId;joinForeignKey:RoleId;refrences:AccessId;joinRefrences:AccessId"`
-  Users []User `gorm:"many2many:user_role;foreignKey:RoleId;joinForeignKey:role_id;refrences:ID;joinRefrences:user_id"`
+  Accesses []Access `gorm:"many2many:role_access;foreignKey:RoleId;joinForeignKey:RoleId;references:AccessId;joinReferences:AccessId"`
+  Users []User `gorm:"many2many:user_role;foreignKey:RoleId;joinForeignKey:role_id;references:ID;joinReferences:user_id"`
 }
