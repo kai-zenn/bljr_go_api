20bcff34b05e2091b52e5895cb1504df5ba8f2db
Kaizen <gibrantsyareza@gmail.com>
2026-06-08 20:24:17 +0700

    adding much more comprehensive error handling for loading the env variable

--- api/configs/dbConf.go
+++ api/configs/dbConf.go
@@ -13,7 +13,10 @@ var DB *gorm.DB
 func InitDB() {
     err := godotenv.Load()
     if err != nil {
-        log.Fatal("Error loading .env file")
+        err = godotenv.Load("../../.env")
+        if err != nil {
+            log.Println("Error loading .env file, menggunakan env bawaan sistem")
+        }
     }
 
     dsn := "host=" + os.Getenv("DB_HOST") + " user=" + os.Getenv("DB_USER") + " password=" + os.Getenv("DB_PASSWORD") + " dbname=" + os.Getenv("DB_NAME") + " port=" + os.Getenv("DB_PORT") + " sslmode=" + os.Getenv("SSL_MODE")
